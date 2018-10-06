aa1b5aa8199eb0c92d9a8b6c8c20218bab08cf8f
Eduardo Moll <egmoll7@gmail.com>
2018-10-06 12:06:33 -0500

    Adds dataDetectorTypes to the message text view

--- EMAlertController/EMAlertController/EMAlertController.swift
+++ EMAlertController/EMAlertController/EMAlertController.swift
@@ -90,7 +90,6 @@ open class EMAlertController: UIViewController {
     textview.textColor = UIColor.black
     textview.backgroundColor = UIColor.clear
     textview.isScrollEnabled = false
-    textview.isSelectable = false
     textview.bounces = false
     
     return textview
@@ -199,6 +198,21 @@ open class EMAlertController: UIViewController {
     }
   }
   
+  /// A Boolean value indicating whether the message text is selectable
+  public var isMessageSelectable: Bool = false {
+    willSet {
+      messageTextView.isSelectable = newValue
+    }
+  }
+  
+  /// Defines the types of information that can be detected in the message text
+  public var dataDetectorTypes: UIDataDetectorTypes? {
+    willSet {
+      guard let newValue = newValue else { return }
+      messageTextView.dataDetectorTypes = newValue
+    }
+  }
+  
   // MARK: - Initializers
   required public init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
@@ -218,6 +232,7 @@ open class EMAlertController: UIViewController {
     
     titleText = title
     messageText = message
+    messageTextView.isSelectable = isMessageSelectable 
     
     setUp()
   }
@@ -393,6 +408,7 @@ extension EMAlertController {
     buttonStackView.axis = .vertical
   }
   
+  ///
   open func addTextField(_ configuration: (_ textField: UITextField?) -> ()) {
     let textField = EMAlertTextField()
     textField.delegate = self
