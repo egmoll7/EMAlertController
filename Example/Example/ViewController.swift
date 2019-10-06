7b29411975a0a4a9f9682c504251faf245e60f18
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2019-10-06 03:28:10 +0300

    Added swift pm

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import EMAlertController
 
 class ViewController: UIViewController {
   
---
+++ Example/Pods/Target Support Files/EMAlertController/EMAlertController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EMAlertController : NSObject
+@end
+@implementation PodsDummy_EMAlertController
+@end
